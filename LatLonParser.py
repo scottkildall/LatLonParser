d66ffc59aecfcadf8a3724a66afa5cb3a53128b5
Scott Kildall <scottkildall@Scotts-MacBook-Pro.local>
2017-11-13 09:40:15 -0800

    sample datasets with printout code

---
+++ LatLonParser.py
@@ -0,0 +1,19 @@
+# LatLonParser
+# Written by Scott Kildall
+
+
+
+
+# initialize the pygame environment, which we use to display an image
+def process(inputFilename, outputFilename):
+	print inputFilename
+	print outputFilename
+
+
+
+# execution point
+# input file = input.csv
+# output file = output.csv
+process("input.csv", "output.csv")
+
+
